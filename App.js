0d41584c8c50a688ba9832bc33b93220dc379f26
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-09-30 21:53:48 +0300

    Kontaktien haku puhelimesta toteutettu

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { useState } from 'react';
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.PhoneNumbers] }
+      );
+      if (data.length > 0) {
+        console.log(data[1]);
+        setContacts(data);
+      }
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => (
+          <View style={styles.flatlistItems}>
+            <Text>{item.name}   </Text>
+            <Text>{item.phoneNumbers?.[0]?.['number']}</Text>
+          </View>
+        )}
+      />
+      <Button title="Get contacts" onPress={getContacts} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flatlistItems: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 });
